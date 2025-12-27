c57643f4db783562d18445207bbb942fd2ae53c7
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-12-27 18:58:20 +0900

    [modify] Modify to use files/setup_comfyui.sh

--- 5.Start_ComfyUI.bat
+++ 5.Start_ComfyUI.bat
@@ -3,8 +3,8 @@ wsl.exe -d Ubuntu24.04-ComfyUI -- bash -lc "tmux new -d -s comfyui '/workspace/c
 wsl.exe -d Ubuntu24.04-ComfyUI -- tmux ls
 
 REM Bootstrap delay to allow ComfyUI to start
-REM Sleep for 10 seconds
-timeout /t 10 /nobreak >nul
+REM Sleep for 5 seconds
+timeout /t 5 /nobreak >nul
 
 REM Open ComfyUI in the default web browser
 start "" "http://localhost:8188" >nul 2>&1
